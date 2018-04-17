47c8a4e2e00be6081a09a7243026cd769d15d64e
Jordan Bowman <desk@jrdnbwmn.com>
2018-04-17 13:02:47 -0600

    Move InscrybMDE into my own code, some customization

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,96 @@
+// InscrybMDE
 var inscrybmde = new InscrybMDE({
-    placeholder: "Type here...",
     spellChecker: false,
     autofocus: true,
     hideIcons: ["guide", "fullscreen", "side-by-side"],
     showIcons: ["strikethrough", "code"],
 });
+
+// Caret
+// https://codepen.io/MContagious/pen/RLMJXR
+function makeCustomCaretInput(inputBlock) {
+  var blockElement = document.getElementById(inputBlock)
+  var label = null;
+  var falseInput = null;
+  var hiddenInput = null;
+  
+  for(var i=0; i<blockElement.children.length; i++) {
+    var elem = blockElement.children[i]
+    if (elem.tagName === "LABEL") label = elem
+    else if (elem.tagName === "SPAN") falseInput = elem
+    else if (elem.tagName === "INPUT") hiddenInput = elem
+  }
+  
+  if (!label || !falseInput || !hiddenInput) {
+    throw new Error("Given element is not having all mandatory child elements.")
+  }
+  
+  // show the same text as input
+  addListenerMulti(falseInput, "click", function() {
+    hiddenInput.focus()
+  })
+  addListenerMulti(hiddenInput, 'keyup mouseup mouseleave', function() {
+    var value = hiddenInput.value;
+    hiddenInput.focus()
+    var caretPos = getCaretPos(hiddenInput)
+    var vals = (value + " ").split("")
+    
+    while (falseInput.firstChild) {
+        falseInput.removeChild(falseInput.firstChild);
+    }
+    for (var i=0; i<vals.length; i++) {
+        var el = document.createElement("span");
+        el.innerText = vals[i];
+        if (i === caretPos) {
+          el.className = 'caret-position'
+        }
+        falseInput.append(el)
+        el.addEventListener('click', (function(i) {
+            
+           return function(event) {
+             event.stopPropagation();
+             var sides = event.target.getBoundingClientRect();
+             var x = (event.clientX - sides.left) + window.pageXOffset;
+             console.log(x + " " + (x<1.5 ? i : i+1))
+             setCaretPos(hiddenInput, (x<1.5 ? i : i+1));
+             hiddenInput.dispatchEvent(new Event('keyup'))
+           }
+        })(i))
+    }
+    
+  });
+}
+
+function addListenerMulti(element, eventNames, listener) {
+  var events = eventNames.split(' ');
+  for (var i=0, iLen=events.length; i<iLen; i++) {
+    element.addEventListener(events[i], listener, false);
+  }
+}
+
+function getCaretPos(input) {  
+    var caret_pos = input.selectionStart;
+    return caret_pos;
+}
+function setCaretPos(elem, caretPos) {
+  
+    if(elem != null) {
+        if(elem.createTextRange) {
+            var range = elem.createTextRange();
+            range.move('character', caretPos);
+            range.select();
+        }
+        else {
+            if(elem.selectionStart) {
+                elem.focus();
+                elem.setSelectionRange(caretPos, caretPos);
+            }
+            else
+                elem.focus();
+        }
+    }
+}
+document.addEventListener("DOMContentLoaded", function(){
+  makeCustomCaretInput("input1-block")
+});
+
