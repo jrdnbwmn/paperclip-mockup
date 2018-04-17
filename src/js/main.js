efed160081ddc094e4c54c05d4bfb993f060a20c
Jordan Bowman <desk@jrdnbwmn.com>
2018-04-17 16:58:25 -0600

    Remove all excess MDE stuff

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,28 @@
 var inscrybmde = new InscrybMDE({
     spellChecker: false,
     autofocus: true,
-    initialValue: "# header",
-    hideIcons: ["guide", "fullscreen", "side-by-side"],
-    showIcons: ["strikethrough", "code"],
+    toolbar: false,
+    status: false,
+    initialValue: "We were fifteen by then, already into our last year at the school. We'd been in the pavilion getting ready for a game of **rounders**. The boys were going *through* a phase of enjoying rounders in order to flirt with us, so there were over thirty of us that afternoon. The ~downpour~ had started while we were ~~changing~~, and we found ourselves gathering on the veranda – which was sheltered by the [pavilion](jrdnbwmn.com) roof – while we waited for it to `stop`.",
 });
+
+// some stuff for toolbar and statusbar
+// hideIcons: ["guide", "fullscreen", "side-by-side", "preview"],
+//     showIcons: ["strikethrough", "code"],
+//     status: ["autosave", {
+//               className: "characters",
+//               defaultValue: function(el) {
+//                 el.innerHTML = "0";
+//               },
+//               onUpdate: function(el) {
+//                 el.innerHTML = simplemde.value().length;
+//               }
+//             }, "words"]
+
+
+
+
+
 
 // Caret
 // https://codepen.io/MContagious/pen/RLMJXR
